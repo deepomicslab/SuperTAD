e54200688b20a3c91a05721b5c54fe921684c589
wang mengbo <mbwang2016@gmail.com>
2020-07-31 13:30:14 +0800

    remove filter.cpp filter.h

--- filter.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by mengbowang on 7/30/2020.
-//
-
-#include "filter.h"
--- filter.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by mengbowang on 7/30/2020.
-//
-
-#ifndef PROGRAM_FILTER_H
-#define PROGRAM_FILTER_H
-
-#endif //PROGRAM_FILTER_H
